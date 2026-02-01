0f2a9ea4b522c1b20fe9d98e662de46c309e33b4
Medeu228 <iakubovmedeu@gmail.com>
2026-02-02 00:26:48 +0500

    Drones

---
+++ src/main/java/edu/narxoz/galactic/edu.narxoz.galactic.drones/Drone.java
@@ -0,0 +1,35 @@
+package edu.narxoz.galactic.edu.narxoz.galactic.drones;
+
+public abstract class Drone {
+
+    private String id;
+    private DroneStatus status;
+    private double maxPayloadKg;
+
+    protected Drone(String id, double maxPayloadKg) {
+        if (maxPayloadKg <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.id = id;
+        this.maxPayloadKg = maxPayloadKg;
+        this.status = DroneStatus.IDLE;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public DroneStatus getStatus() {
+        return status;
+    }
+
+    public double getMaxPayloadKg() {
+        return maxPayloadKg;
+    }
+
+    protected void setStatus(DroneStatus status) {
+        this.status = status;
+    }
+
+    public abstract double speedKmPerMin();
+}
---
+++ src/main/java/edu/narxoz/galactic/edu.narxoz.galactic.drones/DroneStatus.java
@@ -0,0 +1,6 @@
+package edu.narxoz.galactic.edu.narxoz.galactic.drones;
+
+public enum DroneStatus {
+    IDLE,
+    IN_FLIGHT
+}
